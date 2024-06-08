1d940c39f61a2344b658f19e127bb15fb6193a8e
godhani gopal <godhanigopal@outlook.com>
2024-06-07 21:34:46 -0400

    correcting limit parameter

--- src/main/java/com/googleTrendsBigQuery/googleTrendsRestApis/GoogleTrendsRestApisApplication.java
+++ src/main/java/com/googleTrendsBigQuery/googleTrendsRestApis/GoogleTrendsRestApisApplication.java
@@ -1,29 +1,11 @@
 package com.googleTrendsBigQuery.googleTrendsRestApis;
 
-import com.googleTrendsBigQuery.googleTrendsRestApis.service.InternationalTopRisingTermsService;
-<<<<<<< HEAD
-import com.googleTrendsBigQuery.googleTrendsRestApis.util.FileDownloaderUtility;
-=======
->>>>>>> 936544c (get top terms by country name)
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GoogleTrendsRestApisApplication {
-
-<<<<<<< HEAD
-=======
-    @Autowired
-    private InternationalTopRisingTermsService bigQueryService;
-
->>>>>>> 936544c (get top terms by country name)
     public static void main(String[] args) {
         SpringApplication.run(GoogleTrendsRestApisApplication.class, args);
     }
-
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 936544c (get top terms by country name)
--- src/main/java/com/googleTrendsBigQuery/googleTrendsRestApis/repository/QueryBuilderRepository.java
+++ src/main/java/com/googleTrendsBigQuery/googleTrendsRestApis/repository/QueryBuilderRepository.java
@@ -1,5 +1,8 @@
 package com.googleTrendsBigQuery.googleTrendsRestApis.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface QueryBuilderRepository {
     String getTopRisingTermsInternationalQuery(String countryName, int limit);
 }
